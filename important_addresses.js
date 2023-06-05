a914038e974985a83baacdae2bc0d2474d059c24
Messerblatt <markusmeyer2000@protonmail.com>
2023-06-05 18:04:46 +0200

    Added Winners 24H  Changes to be committed: 	new file:   important_addresses.js 	modified:   index.html 	modified:   style.css

---
+++ important_addresses.js
@@ -0,0 +1,399 @@
+var data = {
+	"Routers": [
+	  {
+		"name": "Pancakeswap",
+		"address": "0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898"
+	  },
+	  {
+		"name": "UniswapV2Router02",
+		"address": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+	  }
+	],
+	"Sushiswap": [
+	  {
+		"name": "MasterChef",
+		"address": "0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "MasterChefV2",
+		"address": "0xef0881ec094552b2e128cf945ef17a6752b4ec5d",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiV2Factory",
+		"address": "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiSwapRouter",
+		"address": "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "BentoBoxV1",
+		"address": "0xF5BCE5077908a1b7370B9ae04AdC565EBd643966",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "PeggedOracleV1",
+		"address": "0x6cbfbB38498Df0E1e7A4506593cDB02db9001564",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "KashiPairMediumRisk",
+		"address": "0x2cBA6Ab6574646Badc84F0544d05059e57a5dc42",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiBar",
+		"address": "0x36e2FCCCc59e5747Ff63a03ea2e5C0c2C14911e7",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiMaker",
+		"address": "0x5ad6211CD3fdE39A9cECB5df6f380b8263d1e277",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiMakerKashi",
+		"address": "0x08C82f7513C7952A95029FE3B1587B1FA52DACed",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiRoll",
+		"address": "0x16E58463eb9792Bc236d8860F5BC69A81E26E32B",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiToken",
+		"address": "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "SushiXSwap",
+		"address": "0x011E52E4E40CF9498c79273329E8827b21E2e581",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "WethMaker",
+		"address": "0x5ad6211CD3fdE39A9cECB5df6f380b8263d1e277",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "FuroStream",
+		"address": "0x4ab2FC6e258a0cA7175D05fF10C5cF798A672cAE",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "FuroStreamRouter",
+		"address": "0xf30CF8D373D78D7964A2F7793b730BF7ae0970a8",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "FuroVesting",
+		"address": "0x0689640d190b10765f09310fCfE9C670eDe4E25B",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"name": "FuroVestingRouter",
+		"address": "0x312A8E33d78d3Ab79E62971E86e5e8c9c5E28D64",
+		"imagePath": "/placeholder.png"
+	  },
+	  {
+		"RouteProcessor": "0xF70c086618dcf2b1A461311275e00D6B722ef914",
+		"name": "RouteProcessor",
+		"address": "0xF70c086618dcf2b1A461311275e00D6B722ef914",
+		"imagePath": "/placeholder.png"
+	  }
+	],
+	"ETHUSDOracles": [
+	  {
+		"name": "01node",
+		"address": "0xCF4Be57aA078Dc7568C631BE7A73adc1cdA992F8",
+		"imagePath": "/01node.png"
+	  },
+	  {
+		"address": "0x5a8216a9c47ee2e8df1c874252fdee467215c25b",
+		"imagePath": "/alphachain.png",
+		"name": "Alpha Chain"
+	  },
+	  {
+		"address": "0x57cd4848b12469618b689163f507817940acca02",
+		"imagePath": "/blockdeamon.png",
+		"name": "Blockdaemon"
+	  },
+	  {
+		"address": "0x7663c5790e1ebf04197245d541279d13f3c2f362",
+		"imagePath": "/blocksizecapital.png",
+		"name": "Blocksize Capital"
+	  },
+	  {
+		"address": "0xc74ce67bfc623c803d48afc74a09a6ff6b599003",
+		"imagePath": "/chainlayer.png",
+		"name": "Chainlayer"
+	  },
+	  {
+		"address": "0xb976d01275b809333e3efd76d1d31fe9264466d0",
+		"imagePath": "/dextrac.png",
+		"name": "DexTrac"
+	  },
+	  {
+		"address": "0x5565b5362ff9f468ba2f144f38b87187c9a010a8",
+		"imagePath": "/easy2stake.png",
+		"name": "Easy 2 stake"
+	  },
+	  {
+		"address": "0x218b5a7861dbf368d09a84e0dbff6c6ddbf99db8",
+		"imagePath": "/fiews.png",
+		"name": "Fiews"
+	  },
+	  {
+		"address": "0xedbed9f5dea03dd0ec484577c41502af68b7c46a",
+		"imagePath": "/figmentnetworks.png",
+		"name": "Figment Networks"
+	  },
+	  {
+		"address": "0x2a4a7afa40a9d03b425752fb4cfd5f0ff5b3964c",
+		"imagePath": "/frameworkVentures.png",
+		"name": "Framework Ventures"
+	  },
+	  {
+		"address": "0x982fa4d5f5c8c0063493abe58967ca3b7639f10f",
+		"imagePath": "/infinitystones.png",
+		"name": "Infinity Stones"
+	  },
+	  {
+		"address": "0xf16e77a989529aa4c58318acee8a1548df3fccc1",
+		"imagePath": "/kytzu.png",
+		"name": "Kytzu"
+	  },
+	  {
+		"address": "0x3c4ad65f5b4884397e1f09596c7ac7f8f95b3ff3",
+		"imagePath": "/linkforest.png",
+		"name": "LinkForest"
+	  },
+	  {
+		"address": "0xcc29be4ca92d4ecc43c8451fba94c200b83991f6",
+		"imagePath": "/linkpool.png",
+		"name": "LinkPool"
+	  },
+	  {
+		"address": "0x686bec83b59f8b23a6129f03550a3aad245a543c",
+		"imagePath": "/linkriver.png",
+		"name": "LinkRiver"
+	  },
+	  {
+		"address": "0xf34ac04a28f7cb5324a167c96b24ade9c742b44f",
+		"imagePath": "/mycelium.png",
+		"name": "Mycelium"
+	  },
+	  {
+		"address": "0x9cfab1513ffa293e7023159b3c7a4c984b6a3480",
+		"imagePath": "/newRoadNetwork.png",
+		"name": "Newroad Network"
+	  },
+	  {
+		"address": "0x632f869a26ab4da58e2da476ee74800f2bae060a",
+		"imagePath": "/northwestnodes.png",
+		"name": "NorthWest Nodes"
+	  },
+	  {
+		"address": "0xf07131f578a5f708ae2ccb9faf98458099e0ffb4",
+		"imagePath": "/omniscience.png",
+		"name": "Omniscience"
+	  },
+	  {
+		"address": "0x8f3ab0e87b70a57bd4980111a99a1b2c4b8334f4",
+		"imagePath": "/p2porg.png",
+		"name": "P2P.org"
+	  },
+	  {
+		"address": "0x8b1d49a93a84b5da0917a1ed42d8a3e191c28524",
+		"imagePath": "/prophet.png",
+		"name": "Prophet"
+	  },
+	  {
+		"address": "0x61317c73d0225b2e37140fb9664d607b450613c6",
+		"imagePath": "/symplyvc.png",
+		"name": "Simply VC"
+	  },
+	  {
+		"address": "0x7bfb89db2d7217c57c3ad3d4b55826efd17dc2e9",
+		"imagePath": "/snz.png",
+		"name": "SNZPool"
+	  },
+	  {
+		"address": "0xfa0e4f48a369bb3ecbcee0b5119379ea8d1bcf29",
+		"imagePath": "/stakefish.png",
+		"name": "stake.fish"
+	  },
+	  {
+		"address": "0x43793ee58e0a3d920e3e4a115a9fa07dc4b09715",
+		"imagePath": "/stakingfacilities.png",
+		"name": "Staking Facilities"
+	  },
+	  {
+		"address": "0x0312ea121df0a323ff535b753172736cc9d53d13",
+		"imagePath": "/syncnode.png",
+		"name": "SyncNode"
+	  },
+	  {
+		"address": "0xddeb598fe902a13cc523aaff5240e9988edce170",
+		"imagePath": "/telekom.png",
+		"name": "T-Systems"
+	  },
+	  {
+		"address": "0xc4b732fd121f2f3783a9ac2a6c62fd535fd13fda",
+		"imagePath": "/validationCloud.png",
+		"name": "Validation Cloud"
+	  },
+	  {
+		"address": "0x5a6fcc02d8c50ea58a22115a7c4608b723030016",
+		"imagePath": "/wetez.png",
+		"name": "Wetez"
+	  }
+	],
+	"Balancer": {
+	  "Ethereum": "0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21"
+	},
+	"Curv.fi": {
+	  "Ethereum": {
+		"bCrv Token": "0x3B3Ac5386837Dc563660FB6a0937DFAa5924333B",
+		"BUSD Deposit": "0xb6c057591E073249F2D9D88Ba59a46CFC9B59EdB",
+		"bUSD Liquidity": "Gauge 0x69Fb7c45726cfE2baDeE8317005d3F94bE838840",
+		"BUSD Swap": "0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27",
+		"cCRV Liquidity": "Gauge 0x7ca5b0a2910B33e9759DC7dDB0413949071D7575",
+		"cCrv Token": "0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2",
+		"Compound Deposit": "0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06",
+		"CRV Token": "0xD533a949740bb3306d119CC777fa900bA034cd52",
+		"REN Swap": "0x93054188d876f558f4a66B2EF1d97d16eDf0895B",
+		"EIP20": "0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8"
+	  }
+	},
+	"OpenSea": {
+	  "Ethereum": {
+		"Conduit": "0x1E0049783F008A0085193E00003D00cd54003c71",
+		"ENS Resolver": "0x9C4e9CCE4780062942a7fe34FA2Fa7316c872956",
+		"OPENSTORE TOKEN": "0x495f947276749Ce646f68AC8c248420045cb7b5e",
+		"Payments": "0x18c2E87D183C5338A9142f97dB176F3832b1D6DE",
+		"Registry": "0xa5409ec958C83C3f309868babACA7c86DCB077c1",
+		"Royalties Distributor": "0x9B814233894Cd227f561B78Cc65891AA55C62Ad2",
+		"Transfer Helper": "0x0000000000c2d145a2526bD8C716263bFeBe1A72",
+		"Wyvner Exchange v1": "0x7Be8076f4EA4A4AD08075C2508e481d6C946D12b",
+		"Wyvner Exchange v2": "0x7f268357A8c2552623316e2562D90e642bB538E5",
+		"SeaDrop": "0x00005EA00Ac477B1030CE78506496e8C2dE24bf5"
+	  },
+	  "tokens": {
+		"arbitrum": "0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1",
+		"Tron": "0xE1Be5D3f34e89dE342Ee97E6e90D405884dA6c67",
+		"Shiba Inu": "",
+		"Dai": "",
+		"Render TokenThe Sandbox": "",
+		"Loopring": "",
+		"Mask Network": "",
+		"Enjin Coin": "",
+		"JasmyCoin": "",
+		"CekerBetwork": "",
+		"Aragon": "",
+		"Cartesi": "",
+		"Axelar": "",
+		"Stargate Finance": "",
+		"iExec RLC": "",
+		"Wrapped Beacon Ethereum": "",
+		"OriginTrail": ""
+	  }
+	},
+	"chainlink": {
+	  "Chainlink_ETHUSD_Kovan": "0x9326BFA02ADD2366b30bacB125260Af641031331",
+	  "AAVE_WETHGateway_KOVAN": "0xf8aC10E65F2073460aAD5f28E1EABE807DC287CF",
+	  "SepoliaFaucet_ETH_LINK": "https://sepolia.etherscan.io/address/0x4281ecf07378ee595c564a59048801330f3084ee"
+	},
+	"solidityBugs": [
+	  {
+		"name": "DirtyBytesArrayToStorage",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "ABIDecodeTwoDimensionalArrayMemory",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "EmptyByteArrayCopy",
+		"severity": "medium-severity",
+		"link": ""
+	  },
+	  {
+		"name": "ImplicitConstructorCallvalueCheck",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "TupleAssignmentMultiStackSlotComponents",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "MemoryArrayCreationOverflow ",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "privateCanBeOverridden",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "DynamicArrayCeanup",
+		"severity": "medium-severity",
+		"link": ""
+	  },
+	  {
+		"name": "privateCanBeOverridden",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "SignedArrayStorageCopy",
+		"severity": "low/medium-severity",
+		"link": ""
+	  },
+	  {
+		"name": "ABIEncoderV2StorageArrayWithMultiSlotElement",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "DynamicConstructorArgumentsClippedABIV2",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "UninitializedFunctionPointerInConstructor_0.4.x",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "IncorrectEventSignatureInLibraries_0.4.x",
+		"severity": "very low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "ABIEncoderV2PackedStorage_0.4.x",
+		"severity": "low-severity",
+		"link": ""
+	  },
+	  {
+		"name": "ExpExponentCleanup",
+		"severity": "medium/high-severity",
+		"link": ""
+	  },
+	  {
+		"name": "EventStructWrongData",
+		"severity": "very low-severity",
+		"link": ""
+	  }
+	]
+};
